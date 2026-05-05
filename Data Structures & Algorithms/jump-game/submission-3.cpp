40d7a65d02f329844e992c92258aabd52cf184a7
Trenston Ricks <trenstonricks@gmail.com>
2026-05-05 15:24:38 -0700

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size() - 1;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (nums[i] >= goal - i) goal = i;
+        }
+
+        return goal ? false : true;
+    }
+};
